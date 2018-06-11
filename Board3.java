6076be57903d8dae3f41c0afd76c9852cb96b1b7
herasake <pukuogatatsu@yahoo.co.jp>
2018-06-11 21:39:41 +0900

    Add files via upload

---
+++ Board3.java
@@ -0,0 +1,42 @@
+// Board3.java
+
+import javax.swing.*;
+import java.lang.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+class  Board3 extends JFrame {
+
+  private final int tate = 8;
+  private final int yoko = 8;
+  private final int size = 50;
+  private final int padding = 25;
+
+  private JButton buttonArray[][] = new JButton[ tate ][ yoko ];
+  private Container c;
+
+  Board3() {
+    this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    this.setTitle( "Board3" );
+    this.setSize( ( size * yoko ) + ( padding * 2 ), ( size * tate ) + ( padding * 2 ) );
+    this.setLocationRelativeTo( null );
+    c = this.getContentPane();
+    c.setLayout( null );
+
+    for ( int i = 0 ; i < tate ; i++ ) {
+      for ( int j = 0 ; j < yoko ; j++ ) {
+        buttonArray[ i ][ j ] = new JButton( Integer.toString( ( yoko  * i  ) + j ) );
+        c.add( buttonArray[ i ][ j ] );
+        buttonArray[ i ][ j ].setBounds( size * j + padding, size * i + padding, size, size );
+      }
+    }
+  }
+
+  public static void main( String[] args ) {
+    Board3 gui = new Board3();
+    gui.setVisible( true );
+  }
+
+}
+
